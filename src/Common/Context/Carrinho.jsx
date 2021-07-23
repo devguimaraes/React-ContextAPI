e6cff91c87a4e51ec6d0706e3691c8309d3ec76a
Luiz <luiz.ribeiro@hotmart.com>
2021-07-23 17:37:56 -0300

    login disabled

--- src/Common/Context/Carrinho.jsx
+++ src/Common/Context/Carrinho.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useContext, useState } from "react";
 
 export const CarrinhoContext = createContext();
 CarrinhoContext.displayName = "Carrinho";
@@ -10,6 +10,38 @@ export const CarrinhoProvider = ({ children }) => {
       {children}
     </CarrinhoContext.Provider>
   );
+};
+
+export const useCarrinhoContext = () => {
+  const { carrinho, setCarrinho } = useContext(CarrinhoContext);
+
+  const adicionarProduto = (novoProduto) => {
+    const verificaSeProdutoExiste = carrinho.some(
+      (itemDoCarrinho) => itemDoCarrinho.id === novoProduto.id
+    );
+    if (!verificaSeProdutoExiste) {
+      novoProduto.quantidade = 1;
+      return setCarrinho((carrinhoAnterior) => [
+        ...carrinhoAnterior,
+        novoProduto,
+      ]);
+    }
+
+    setCarrinho((carrinhoAnterior) =>
+      carrinhoAnterior.map((itemDoCarrinho) => {
+        if (itemDoCarrinho.id === novoProduto.id) {
+          itemDoCarrinho.quantidade += 1;
+        }
+        return itemDoCarrinho;
+      })
+    );
+  };
+
+  return {
+    carrinho,
+    setCarrinho,
+    adicionarProduto,
+  };
 };
 
 export default CarrinhoProvider;
--- src/components/Produto/index.js
+++ src/components/Produto/index.js
@@ -1,9 +1,9 @@
 import { Container } from './styles';
-import { memo, useContext } from 'react';
+import { memo } from 'react';
 import { IconButton } from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
 import RemoveIcon from '@material-ui/icons/Remove';
-import{ CarrinhoContext } from 'Common/Context/Carrinho'
+import { useCarrinhoContext } from 'Common/Context/Carrinho'
 
 
 function Produto({
@@ -14,30 +14,37 @@ function Produto({
   unidade
 }) {
 
-  const {carrinho, setCarrinho} = useContext(CarrinhoContext)
+  const { carrinho, adicionarProduto } = useCarrinhoContext()
+
+  const produtoNoCarrinho = carrinho.find((itemDoCarrinho)=> itemDoCarrinho.id === id)
+  
 
   return (
-      <Container>
-        <div>
-          <img
-            src={`/assets/${foto}.png`}
-            alt={`foto de ${nome}`}
-          />
-          <p>
-            {nome} - R$ {valor?.toFixed(2)} <span>Kg</span>
-          </p>
-        </div>
-        <div>
-          <IconButton
-            color="secondary"
-          >
-            <RemoveIcon />
-          </IconButton>
-          <IconButton>
-            <AddIcon />
-          </IconButton>
-        </div>
-      </Container>
+    <Container>
+      <div>
+        <img
+          src={`/assets/${foto}.png`}
+          alt={`foto de ${nome}`}
+        />
+        <p>
+          {nome} - R$ {valor?.toFixed(2)} <span>Kg</span>
+        </p>
+      </div>
+      <div>
+        <IconButton
+          disabled={produtoNoCarrinho?.quantidade === 0}
+          onClick={()=>console.log('cliquei')}
+          color="secondary"
+        >
+          <RemoveIcon />
+        </IconButton>
+        {produtoNoCarrinho ? produtoNoCarrinho.quantidade : 0 }
+        <IconButton onClick={() => adicionarProduto({ nome, foto, id, valor })
+        }>
+          <AddIcon />
+        </IconButton>
+      </div>
+    </Container>
   )
 }
 
